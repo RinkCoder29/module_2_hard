0901d35fc4e921a44e2021d9ab96b1c157670a23
RinkCoder29 <vyacheslav.vereshchagin17@gmail.com>
2024-09-17 11:32:57 +0300

    Update module_hard.py

--- module_hard.py
+++ module_hard.py
@@ -2,12 +2,14 @@ import random
 numbers = list(range(3, 21))
 random_num = random.choice(numbers)
 print(f"Рандомное число: {random_num}")
-list_= set()
+list_= []
 for i in range(1, random_num):
     for j in range(1, random_num):
         if (i + j) > 0 and random_num % (i + j) == 0:
             pair = tuple(sorted((i,j)))
             if pair[0] != pair[1] and pair not in list_:
-                list_.add(pair)
-if list:
-    print(f"числа для вставки: {list_}")
+                list_.append(pair)
+list_.sort(key = lambda x: x[0])
+output = ''.join(f'{pair[0]}{pair[1]}' for pair in list_)
+if output:
+    print(f"числа для вставки {random_num} - {output}")
